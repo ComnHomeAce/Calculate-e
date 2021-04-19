0a0c1ffadd563c76c138bd8f5e505d5be821fcce
Chip <59837459+ComnHomeAce@users.noreply.github.com>
2021-04-18 20:50:05 -0700

    Update Calculate e.cpp

--- Calculate e/Calculate e.cpp
+++ Calculate e/Calculate e.cpp
@@ -1,20 +1,45 @@
-// Calculate e.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+// This program will calculate e
 
 #include <iostream>
+using namespace std;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    //n = input
+    //fact = factorial
+    //i = index
+    //e = calculated value
+    long double n, i, fact, e = 1;
+
+    cout << "This program will calculate e (Euler\'" << "s Number) using the below continuous sum:\n\n";
+    cout << "         1    1    1\n";
+    cout << "e = 1 + -- + -- + -- + ...\n";
+    cout << "        1!   2!   3!\n\n";
+
+    cout << "Enter the number of terms to calculate: ";
+    cin >> n;
+
+    i = n;
+    fact = n;
+
+    for (int j = 0; j < n; j++)
+    {
+        //Loops to calculate factorial of n
+        for (int k = 1; k < i; k++)
+        {
+            fact *= (i - k);
+        }
+
+        cout << "fact = " << fact << endl;
+        e += (1 / fact);
+        cout << "e = " << e << endl;
+        cout << "i = " << i << endl;
+        i--;
+        cout << "i = " << i << endl;
+        fact = i;
+        }
+
+    //Output of e
+    cout << "e calculated to " << n << " terms of the infinite sum is " << e << "\n";
+
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
